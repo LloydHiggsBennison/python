c9c48fb32aafc70a427514896df10c30850d1d2e
Lloyd Higgs Bennison <146937784+LloydHiggsBennison@users.noreply.github.com>
2024-07-07 18:26:40 -0400

    Terminado

--- prueba2.py
+++ prueba2.py
@@ -1,26 +1,13 @@
 from funciones import mainMenu, agregarLibro, actualizarLibro, buscarLibro, listarLibros, eliminarLibro
 from listaLibros import lista_libros
 import os
-# agregar Libros, atraves de una funcion (titulo, autor, año de publicacion), utilizando una lista para almacenar diccionarios, cada diccionario es un libro.
-# verificar año de publicacion que sea numero positivo, y generar un mensaje de error si no es un numero positivo.
 
-# Actualizar informacion de Libros (recibe el titulo de libro, el nuevo titulo, autor y año de publicacion), 
-# si el libro no existe mostrar mensaje de error. manejo de errores posibles.
-
-# Buscar libros por titulo:
-# funcion debe recibir el nombre del titulo y devolver informacion completa del libro si es que existe, si no existe mostrar mensaje de error.
-
-# Listar todos los libros
-# La función debe imprimir una lista ordenada de todos los libros, mostrando su título, autor y año de publicación.
-
-# Eliminar libros
-# La función debe recibir el título del libro y eliminarlo de la biblioteca si existe. Si no existe, debe mostrar un mensaje de error.
-
-# Manejo de errores como entrada de datos no validos, manejo de excepciones con try-except.
-
+################################################################
+# Menu principal
 
 while True:
     mainMenu()
+    print("")
     opcion = input("Ingresa el numero de la opcion: ")
     if opcion == "1":
         agregarLibro()
@@ -39,3 +26,5 @@ while True:
         print("Ingresa una opcion valida.")
         input("Presiona enter para continuar: ")
         os.system("cls")
+
+################################################################
